fa4d9a69b33139ee378f4b61ca365e1e6b5d236c
N0dnarbs <bvanharen1981@gmail.com>
2024-05-03 15:33:35 -0400

    Lesson 124 done and working

--- Source/Aura/Public/AuraEnemy.h
+++ Source/Aura/Public/AuraEnemy.h
@@ -5,9 +5,10 @@
 #include "CoreMinimal.h"
 #include "AuraCharacterBase.h"
 #include "Interaction/EnemyInterface.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
 #include "AuraEnemy.generated.h"
 
-
+class UWidgetComponent;
 /**
  * 
  */
@@ -27,10 +28,19 @@ public:
 	virtual int32 GetPlayerLevel() override;
 	/** Combat Interface */
 
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChangedSignature OnHealthChanged;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnAttributeChangedSignature OnMaxHealthChanged;
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
 	int32 Level = 1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UWidgetComponent> HealthBar;
 };
