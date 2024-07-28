8ab346f04f32db62af3feb09e4427bee45b1be7d
N0dnarbs <bvanharen1981@gmail.com>
2024-07-28 15:00:10 -0400

    Lesson 150 done!

---
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -0,0 +1,18 @@
+
+
+
+#include "AuraAbilityTypes.h"
+
+//AuraAbilityTypes::AuraAbilityTypes()
+//{
+//}
+
+//AuraAbilityTypes::~AuraAbilityTypes()
+//{
+//}
+
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+
+	return true;
+}
